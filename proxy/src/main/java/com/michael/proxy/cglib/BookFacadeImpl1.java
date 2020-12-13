6025664c224da32e8c44af8015bc25acb3955931
Michael <13432849094@163.com>
2020-12-13 21:52:21 +0800

    cglib 添加获取 target 对象的方法

--- proxy/src/main/java/com/michael/proxy/cglib/BookFacadeImpl1.java
+++ proxy/src/main/java/com/michael/proxy/cglib/BookFacadeImpl1.java
@@ -3,7 +3,15 @@ package com.michael.proxy.cglib;
 public class BookFacadeImpl1 { 
 	
     public void addBook() {  
-        System.out.println("新增图书...");  
-    }  
+        System.out.println("新增图书...");
+    }
+
+    public void delBook() {
+        System.out.println("删除图书...");
+    }
+
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
 }  
 
